62111b79591e4ee276f4c13729b3ee94b2083d51
PanBazant <PanBazant@users.noreply.github.com>
2023-02-16 23:10:27 +0100

    day 22

--- 21.py
+++ 21.py
@@ -1,4 +1,4 @@
 piano_keys = ["a", "b", "c", "d", "e", "f", "g"]
 
 print(piano_keys[::3]) # wypisze co drugi element
-print(piano_keys[::-1]) # odwróci kolejność listy 
+print(piano_keys[::-1]) # odwróci kolejność listy git 
